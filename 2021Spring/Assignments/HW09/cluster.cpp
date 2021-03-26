3e57cec396c2432f10f745bcbd75a7321c7b3105
Ruochun <ruochunz@gmail.com>
2021-03-26 16:57:51 -0500

    Change HW09 P1 to use float array

--- 2021Spring/Assignments/HW09/cluster.cpp
+++ 2021Spring/Assignments/HW09/cluster.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include "cluster.h"
 
-void cluster(const size_t n, const size_t t, const int *arr, const int *centers, int *dists) {
-    #pragma omp parallel num_threads(t)
-    {
-        unsigned int tid = omp_get_thread_num();
-        #pragma omp for
-        for (size_t i = 0; i < n; i++) {
-            dists[tid] += abs(arr[i] - centers[tid]);
-        }
+void cluster(const size_t n, const size_t t, const float *arr,
+             const float *centers, float *dists) {
+#pragma omp parallel num_threads(t)
+  {
+    unsigned int tid = omp_get_thread_num();
+#pragma omp for
+    for (size_t i = 0; i < n; i++) {
+      dists[tid] += abs(arr[i] - centers[tid]);
     }
+  }
 }
--- 2021Spring/Assignments/HW09/cluster.h
+++ 2021Spring/Assignments/HW09/cluster.h
@@ -14,12 +14,15 @@
 // t - number of threads.
 // "centers" - an array of local center positions; it has length t.
 // "dists" - an array that stores the calculated distances; it has length t.
+// (if you use padding to resolve the issue then the lengths of these arrays can
+// change accordingly)
 
 // Example input: arr = [0, 1, 3, 4, 6, 6, 7, 8], n = 8, t = 2.
 // centers = [2, 6] (this is calculated in task1.cpp).
 // Expected results: dists = [6, 3].
 // 6 = |0-2| + |1-2| + |3-2| + |4-2|; 3 = |6-6| + |6-6| + |7-6| + |8-6|.
 
-void cluster(const size_t n, const size_t t, const int *arr, const int *centers, int *dists);
+void cluster(const size_t n, const size_t t, const float *arr,
+             const float *centers, float *dists);
 
 #endif
